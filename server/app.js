4c68cf9ea63e000e540829cd1acca5edba43b075
Subham Shrestha <subham.shrestha13@gmail.com>
2024-12-10 10:30:00 +0545

    Initialize backend

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const route = require("./routes/index");
+
+const app = express();
+const port = Number(process.env.PORT);
+
+app.use(cors());
+
+
+app.use(express.json());
+app.use("/", route);
+
+
+
+app.use((err, req, res, next) => {
+  const errorMsg = err ? err.toString() : "Something went wrong";
+  res.status(500).json({ msg: errorMsg });
+});
+
+app.listen(port, () => {
+  console.log(`Appilcation is running at port ${port}`);
+});
