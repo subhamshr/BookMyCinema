7950ac2d81cb27de144c0155b12c337399e3433d
Subham Shrestha <subham.shrestha13@gmail.com>
2024-12-11 10:30:00 +0545

    Connect backend to mongodb

--- server/app.js
+++ server/app.js
@@ -7,7 +7,18 @@ const app = express();
 const port = Number(process.env.PORT);
 
 app.use(cors());
+const mongoose = require("mongoose");
 
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.log("Database Error", error);
+  });
 
 app.use(express.json());
 app.use("/", route);
